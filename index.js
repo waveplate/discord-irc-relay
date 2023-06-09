76b48a99b06537c9cc2d2df94aaad0d87dc9b2fc
Anatoly Bazarov <dissahc@protonmail.com>
2023-06-08 21:28:28 -0400

    multichan support, only create irc client if user is active, exclude nicks

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const IRC = require('irc-framework');
 const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
 
 let bots = {};
-let channel = null;
+let channels = {};
 
-const ircOpts = {
-    host: 'irc.efnet.org',
-    port: 6697,
-    chan:'#discord-relay',
-    maxNickLength: 16,
-}
-
-const discordOpts = {
-    channelId: '', // discord text channel id
-    token: '', // discord bot token
-}
+const config = JSON.parse(fs.readFileSync('config.json'));
 
 const client = new Client({
     intents: [
@@ -25,72 +16,95 @@ const client = new Client({
     ],
 });
 
+client.login(config.discordToken);
+
 client.on('ready', () => {
     console.log(`Bot is ready!`);
 
-    channel = client.channels.cache.get(discordOpts.channelId);
+    Object.values(config.channels).forEach(channelId => {
+        channels[channelId] = client.channels.cache.get(channelId);
+    });
 
-    if (channel) {
-        channel.guild.members.fetch().then(members => {
-            members.forEach(member => {
-                bots[member.user.id] = createBot(member.user.username);
-            });
+    bots[client.user.id] = createBot(client.user.username);
 
-            bots[client.user.id].on('message', function (event) {
-                if(!messageIsFromRelay(event.nick)){
-                    channel.send(`<${event.nick}> ${event.message}`).catch(console.error);
-                }
-            });
-        });
-    }
+    bots[client.user.id].on('message', function (event) {
+        if(config.excludeNicks.includes(event.nick))
+            return;
+
+        let channel = channels[config.channels[event.target]];
+        if (!messageIsFromRelay(event.nick)) {
+            channel.send(`<${event.nick}> ${event.message}`).catch(console.error);
+        }
+    });
 });
 
 client.on('messageCreate', message => {
-    if(bots[message.author.id] == null)
-        bots[message.author.id] = createBot(message.author.username);
+    if (message.author.id == client.user.id)
+        return;
 
-    if(message.author.id != client.user.id){
-        bots[message.author.id].say(ircOpts.chan, message.content);
-        if(message.attachments.size > 0){
-            message.attachments.forEach(attachment => {
-                bots[message.author.id].say(ircOpts.chan, attachment.url);
-            });
-        }
+    if (bots[message.author.id] == null) {
+        bots[message.author.id] = createBot(message.author.username, () => {
+            relayMessage(message);
+        });
+    } else {
+        relayMessage(message);
     }
 });
 
-client.login(discordOpts.token);
-
-function createBot(nick){
+function createBot(nick, registerCallback) {
     console.log(`Creating bot for ${nick}`);
 
     var bot = new IRC.Client();
 
     bot.connect({
-        host: ircOpts.host,
-        port: ircOpts.port,
+        host: config.host,
+        port: config.port,
         nick: sanitizeDiscordUsername(nick),
-        tls: ircOpts.port == 6697 ? true : false,
+        tls: config.port == 6697 ? true : false,
         rejectUnauthorized: false,
     });
 
     bot.on('registered', () => {
-        bot.join(ircOpts.chan);
+        Object.keys(config.channels).forEach(channel => {
+            bot.join(channel);
+        });
+        if (registerCallback != null)
+            registerCallback();
     });
 
     return bot;
 }
 
-function sanitizeDiscordUsername(username){
-    return username.replace(/[^a-zA-Z0-9]/g, '').substring(0, ircOpts.maxNickLength);
+function relayMessage(message) {
+    const ircChannel = getIrcChannelFromDiscordChannel(message.channel.id);
+
+    bots[message.author.id].say(ircChannel, message.content);
+    if (message.attachments.size > 0) {
+        message.attachments.forEach(attachment => {
+            bots[message.author.id].say(ircChannel, attachment.url);
+        });
+    }
 }
 
-function messageIsFromRelay(nick){
+function getIrcChannelFromDiscordChannel(discordChannelId) {
+    let ircChannel = false;
+    Object.keys(config.channels).forEach(ircChannelId => {
+        if (config.channels[ircChannelId] == discordChannelId)
+            ircChannel = ircChannelId;
+    });
+    return ircChannel;
+};
+
+function messageIsFromRelay(nick) {
     let isFromRelay = false;
     Object.values(bots).forEach(bot => {
-        if(bot.options.nick == nick)
+        if (bot.options.nick == nick)
             isFromRelay = true;
     });
 
     return isFromRelay;
+}
+
+function sanitizeDiscordUsername(username) {
+    return username.replace(/[^a-zA-Z0-9]/g, '').substring(0, config.maxNickLength);
 }
